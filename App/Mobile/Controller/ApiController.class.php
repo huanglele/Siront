083c3b9a478ba2843a7a1dba32d6f98587407ce0
huanglele <huanglele@yousuowei.cn>
2016-05-18 15:06:25 +0800

    后台数据

--- App/Mobile/Controller/ApiController.class.php
+++ App/Mobile/Controller/ApiController.class.php
@@ -66,7 +66,6 @@ class ApiController extends Controller
                     $data['deviceId'] = $this->deviceId;
                     $this->uid = $info['uid'];
                     $data['token'] = session_id();
-
                     $Mem = new \Memcache();
                     $Mem->set($info['uid'],$data);
                     $res['status'] = 'success';
