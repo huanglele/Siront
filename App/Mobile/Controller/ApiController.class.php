8de890cb8cc3ffd6e27752aed9369b7292db9658
huanglele <huanglele@yousuowei.cn>
2016-05-23 10:47:25 +0800

    添加测试极光推送API

--- App/Mobile/Controller/ApiController.class.php
+++ App/Mobile/Controller/ApiController.class.php
@@ -172,6 +172,7 @@ class ApiController extends Controller
             ->setMessage("msg content", 'msg title', 'type', array("key1"=>"value1", "key2"=>"value2"))
             ->setOptions(100000, 3600, null, false)
             ->send();
+        echo 'Result=' . json_encode($result);
     }
 
 }
