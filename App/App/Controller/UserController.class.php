765bf0e6e4627be606d7d227528a8c827d828bf2
huanglele <huanglele@yousuowei.cn>
2016-06-01 10:52:58 +0800

    添加退出登录功能

--- App/App/Controller/UserController.class.php
+++ App/App/Controller/UserController.class.php
@@ -81,4 +81,48 @@ class UserController extends Controller
         }
     }
 
+    /**
+     * 提交注册
+     */
+    public function reg(){
+        if(IS_POST){
+            $telCode = I('post.telCode');
+            $res['status'] = 'error';
+            if(session('telCode')==$telCode){
+                $data['phone'] = I('post.phone');
+                $data['password'] = md5(I('post.password'));
+                $data['money'] = $data['coin'] = $data['person_status'] = $data['company_status'] = 0;
+                $data['user_status'] =2;        //注册通过验证
+                $data['create_time'] = time();
+                $data['headimgurl'] =  '';
+                $data['nickname'] = I('post.nickname');
+                $M = D('user');
+                if($M->create($data)){
+                    $r = $M->add($data);
+                    if($r){
+                        session('phone',$data['phone']);
+                        session('uid',$r);
+                        $data['status'] = 'success';
+                        $res['msg'] = '注册成功';
+                    }else{
+                        $res['msg'] = '注册失败请重试';
+                    }
+                }else{
+                    $res['msg'] = $M->getError();
+                }
+            }else{
+                $res['msg'] = '短信验证码错误';
+            }
+            $this->ajaxReturn($res);
+        }
+    }
+
+    /**
+     * 退出登录
+     */
+    public function logout(){
+        session('uid',null);
+        $this->success('安全退出');
+    }
+
 }
